e264e21c86ac6303968a19b9a67aa3f766e1985e
Zyox <zyoxthecoder@gmail.com>
2024-12-22 14:27:15 +1100

    finally got the vscode inline completion working

--- vscode/aphelion/extension.js
+++ vscode/aphelion/extension.js
@@ -10,25 +10,43 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "aphelion" is now active!');
+	console.log('Aphelion autocomplete now active');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('aphelion.helloWorld', function () {
+
+	let disposable = vscode.commands.registerCommand('aphelion.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from aphelion!');
 	});
 
+	const provider = {
+		provideInlineCompletionItems: async (document, position, context, token) => {
+			const line = document.lineAt(position.line);
+			console.log("420")
+			// Here we need to call the python code for generating stuff
+			if (line.text.startsWith('if')) {
+				console.log("421")
+				let range = line.range;
+				if (line.text.indexOf(";") !== -1) {
+					console.log("422")
+					range = new vscode.Range(range.start, range.end.with(undefined, line.text.indexOf(";") + 1));
+				}
+				console.log("423")
+				return [{ text: 'if (hello) {\n};', insertText: "if (hello)", range }];
+			}
+		}
+	};
+	vscode.languages.registerInlineCompletionItemProvider({ pattern: '**' }, provider);
+
 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
